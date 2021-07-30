96e479f641fbdcaec79c7211b1944c0e751d5072
GONZALO ROJAS <rojasg476@gmail.com>
2021-07-30 11:56:00 -0500

    Theming the app (#6)
    theming the app with shopify restyle

---
+++ animeapp/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, SafeAreaView } from 'react-native'
+
+import { Text } from '../constants'
+
+const index = () => {
+    return (
+        <View>
+            <SafeAreaView />
+            <Text variant="largeTitle">SERIES APP SRC</Text>
+        </View>
+    )
+}
+
+export default index
