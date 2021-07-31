4c6e0da87bf602653f353a6fda1f8864a06bf5a4
GONZALO ROJAS <rojasg476@gmail.com>
2021-07-31 14:08:30 -0500

    #21 creating the header component

--- animeapp/constants/images.ts
+++ animeapp/constants/images.ts
@@ -2,10 +2,12 @@ const backgroundImage = require('../assets/images/screenBg.png')
 const noImage = require('../assets/images/noimage.jpeg')
 const profileImage = require('../assets/images/profile.jpg')
 const experienceImageCard = require('../assets/images/experienceImageCard.png')
+const face = require('../assets/images/face.jpg')
 
 export default {
     backgroundImage,
     profileImage,
     noImage,
-    experienceImageCard
+    experienceImageCard,
+    face
 }
